014ec020ce7b4368c3b1f10aa3f3468dab65ccd6
Rodrigo Machado <rodrigo.aocubo@gmail.com>
2012-05-15 15:44:38 +0100

    create actions in project

---
+++ library/C3op/Projects/ActionException.php
@@ -0,0 +1,7 @@
+<?php
+
+
+class C3op_Projects_ProjectException extends RuntimeException {
+    //put your code here
+}
+
---
+++ library/C3op/Projects/ActionMapperException.php
@@ -0,0 +1,7 @@
+<?php
+class C3op_Projects_ProjectMapperException extends RuntimeException 
+{
+    //put your code here
+}
+
+?>
