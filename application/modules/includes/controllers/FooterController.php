d8bd7b1c8904aa94d5ee45282aee02fe59879788
Rodrigo Machado <rodrigo.aocubo@gmail.com>
2012-08-20 13:32:37 -0300

    cleaning MediadorIncludes garbage

--- application/modules/includes/controllers/FooterController.php
+++ application/modules/includes/controllers/FooterController.php
@@ -1,7 +1,5 @@
 <?php
 
-require_once APPLICATION_PATH . "/modules/includes/models/MediadorIncludes.php";
-
 class Includes_FooterController extends Zend_Controller_Action
 {
 
@@ -14,11 +12,6 @@ class Includes_FooterController extends Zend_Controller_Action
 
     public function includeAction()
     {
-        /* Initialize model and retrieve data here */
-        
-        $reqIncludes = new MediadorIncludes();
-
-        
     }
 
 
