c3ab6cc932b0c68c279e2ebf790af4e8c0fe8e96
Rodrigo Machado <rodrigo.aocubo@gmail.com>
2012-09-18 14:31:46 -0300

    show active module at header

--- application/modules/includes/controllers/HeaderController.php
+++ application/modules/includes/controllers/HeaderController.php
@@ -7,18 +7,18 @@ class Includes_HeaderController extends Zend_Controller_Action
     {
         /* Initialize action controller here */
     }
-    
+
     public function preDispatch()
     {
         // check if user is authenticated
         // if not, redirect to login page
-        
+
         if (!Zend_Auth::getInstance()->hasIdentity()) {
             $url = $this->getRequest()->getRequestUri();
-            $action = $this->getRequest()->getActionName();
+            $actionName = $this->getRequest()->getActionName();
             $this->_helper->layout->setLayout('login');
             $session = new Zend_Session_Namespace('c3op.auth');
-            if ($action != "include-login") {
+            if ($actionName != "include-login") {
                 $session->requestURL = $url;
                 $this->_redirect('/auth/login');
             }
@@ -28,11 +28,24 @@ class Includes_HeaderController extends Zend_Controller_Action
 
     public function includeAction()
     {
-        /* Initialize model and retrieve data here */
+        $pageData = Array();
 
-        $dadosPagina = Array();
+        $controller = $this->getFrontController();
+        $moduleName = $controller->getParam('outerModule');
 
-        /* Initialize view and populate here */
+        $navigationTabs = array(
+            'projects' => array(
+                'url' => '/projects',
+                'label' => 'Projetos',
+                'active' => ($moduleName == 'projects' ? true : false)
+            ),
+            'register' => array(
+                'url' => '/register',
+                'label' => 'Cadastro',
+                'active' => ($moduleName == 'register' ? true : false)
+            ),
+        );
+
 
         //saudacao_usuario
         //  logado: verdadeiro/falso
@@ -43,27 +56,30 @@ class Includes_HeaderController extends Zend_Controller_Action
         if ($auth->hasIdentity()) {
             $identity = $auth->getIdentity(); //Identity exists; get it
 
-            $dadosPagina['saudacao_usuario']['logado'] = true;
-            $dadosPagina['saudacao_usuario']['nome_usuario'] = $identity->GetLogin();
-            $dadosPagina['saudacao_usuario']['link_edita'] = "/autenticacao/conta/edita?titulo=" . $identity->GetId();
+            $pageData['saudacao_usuario']['logado'] = true;
+            $pageData['saudacao_usuario']['nome_usuario'] = $identity->GetLogin();
+            $pageData['saudacao_usuario']['link_edita'] = "/autenticacao/conta/edita?titulo=" . $identity->GetId();
 //            $dadosPagina['links_menu']['link_aulas'] = "/planejamento/aula/lista?titulo=" . $identity->apelidoUsuario;
-            $dadosPagina['links_menu']['link_aulas'] = "/projects";
+            $pageData['links_menu']['link_aulas'] = "/projects";
 
         } else {
-            $dadosPagina['saudacao_usuario']['logado'] = false;
-            $dadosPagina['saudacao_usuario']['nome_usuario'] = "";
-            $dadosPagina['saudacao_usuario']['link_edita'] = "";
-            $dadosPagina['links_menu']['link_aulas'] = "/projects";
+            $pageData['saudacao_usuario']['logado'] = false;
+            $pageData['saudacao_usuario']['nome_usuario'] = "";
+            $pageData['saudacao_usuario']['link_edita'] = "";
+            $pageData['links_menu']['link_aulas'] = "/projects";
         }
 
-        $this->view->saudacaoUsuario = $dadosPagina['saudacao_usuario'];
-        $this->view->linksMenu = $dadosPagina['links_menu'];
+        $this->view->saudacaoUsuario = $pageData['saudacao_usuario'];
+        $this->view->linksMenu = $pageData['links_menu'];
+        $this->view->moduleName = $moduleName;
+        $this->view->navigationTabs = $navigationTabs;
+
 
     }
 
     public function includeLoginAction()
     {
-        
+
         /* Initialize model and retrieve data here */
 
         $dadosPagina = Array();
