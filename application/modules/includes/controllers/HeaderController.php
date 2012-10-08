a45aff8ac714357a726ffe6ac80cfeb3e567867e
Rodrigo Machado <rodrigo.aocubo@gmail.com>
2012-10-08 18:13:42 -0300

    removing some blank lines, old code and even a deprecated method

--- application/modules/includes/controllers/HeaderController.php
+++ application/modules/includes/controllers/HeaderController.php
@@ -46,13 +46,6 @@ class Includes_HeaderController extends Zend_Controller_Action
             ),
         );
 
-
-        //saudacao_usuario
-        //  logado: verdadeiro/falso
-        //  nome_usuario: texto
-        //  link_edita: texto
-        $auth = Zend_Auth::getInstance();
-
         if ($auth->hasIdentity()) {
             $identity = $auth->getIdentity(); //Identity exists; get it
 
@@ -67,49 +60,7 @@ class Includes_HeaderController extends Zend_Controller_Action
         }
 
         $pageData['navigationTabs'] = $navigationTabs;
-
-        $this->view->moduleName = $moduleName;
-        $this->view->navigationTabs = $navigationTabs;
         $this->view->pageData = $pageData;
-
-
     }
-
-    public function includeLoginAction()
-    {
-
-        /* Initialize model and retrieve data here */
-
-        $dadosPagina = Array();
-
-        /* Initialize view and populate here */
-
-        //saudacao_usuario
-        //  logado: verdadeiro/falso
-        //  nome_usuario: texto
-        //  link_edita: texto
-        $auth = Zend_Auth::getInstance();
-
-        if ($auth->hasIdentity()) {
-            $identity = $auth->getIdentity(); //Identity exists; get it
-
-            $dadosPagina['saudacao_usuario']['logado'] = true;
-            $dadosPagina['saudacao_usuario']['nome_usuario'] = $identity->nomeUsuario;
-            $dadosPagina['saudacao_usuario']['link_edita'] = "/autenticacao/conta/edita?titulo=" . $identity->apelidoUsuario;
-//            $dadosPagina['links_menu']['link_aulas'] = "/planejamento/aula/lista?titulo=" . $identity->apelidoUsuario;
-            $dadosPagina['links_menu']['link_aulas'] = "/projects";
-
-        } else {
-            $dadosPagina['saudacao_usuario']['logado'] = false;
-            $dadosPagina['saudacao_usuario']['nome_usuario'] = "";
-            $dadosPagina['saudacao_usuario']['link_edita'] = "";
-            $dadosPagina['links_menu']['link_aulas'] = "/projects";
-        }
-
-        $this->view->saudacaoUsuario = $dadosPagina['saudacao_usuario'];
-        $this->view->linksMenu = $dadosPagina['links_menu'];
-
-    }
-
 
 }
