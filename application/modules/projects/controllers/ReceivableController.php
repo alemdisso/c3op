8b861ce1004b525bcec1104d8d94feb9a06528cd
Rodrigo Machado <rodrigo.aocubo@gmail.com>
2012-11-07 15:01:59 -0200

    #59 lacking setdata function

--- application/modules/projects/controllers/ReceivableController.php
+++ application/modules/projects/controllers/ReceivableController.php
@@ -146,4 +146,14 @@ class Projects_ReceivableController extends Zend_Controller_Action
 
     }
 
+    private function setDateValueToFormField(Zend_Form $form, $fieldName, $value)
+    {
+        $field = $form->getElement($fieldName);
+        if ($value != '0000-00-00')  {
+            $field->setValue(C3op_Util_DateDisplay::FormatDateToShow($value));
+        } else {
+            $field->setValue("");
+        }
+    }
+
 }
