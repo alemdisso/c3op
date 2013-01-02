dbd7bd0988ec2c240aa8685c471ecf7da975e749
Rodrigo Machado <rodrigo.aocubo@gmail.com>
2013-01-02 11:08:50 -0200

    #116 deliveries: database patch

--- library/C3op/Access/Acl.php
+++ library/C3op/Access/Acl.php
@@ -67,6 +67,7 @@ public function __construct() {
     $this->allow(C3op_Access_RolesConstants::ROLE_ADMINISTRATOR, 'c3op:projects.outlay', 'success');
     $this->deny(C3op_Access_RolesConstants::ROLE_CONTROLLER,     'c3op:projects.outlay');
 
+    $this->allow(C3op_Access_RolesConstants::ROLE_ASSISTANT,     'c3op:projects.project', 'amend');
     $this->allow(C3op_Access_RolesConstants::ROLE_ASSISTANT,     'c3op:projects.project', 'create');
     $this->allow(C3op_Access_RolesConstants::ROLE_ASSISTANT,     'c3op:projects.project', 'detail');
     $this->allow(C3op_Access_RolesConstants::ROLE_ASSISTANT,     'c3op:projects.project', 'edit');
