897b9860621047b1a439631f69a2ad300c70363e
Rodrigo Machado <rodrigo.aocubo@gmail.com>
2013-02-12 11:29:59 -0200

    #145 resource just has credit if action was accepted

--- library/C3op/Resources/TeamMemberHasCredit.php
+++ library/C3op/Resources/TeamMemberHasCredit.php
@@ -13,32 +13,40 @@ class C3op_Resources_TeamMemberHasCredit {
 
     public function hasCreditToPay()
     {
-        $budget = $this->teamMember->getValue();
-        $payed = $this->mapper->getSumOfPayedOutlays($this->teamMember);
-        $provided = $this->mapper->getSumOfProvidedButNotPayedOutlays($this->teamMember);
+        if ($this->actionHasBeenApproved()) {
+            $budget = $this->teamMember->getValue();
+            $payed = $this->mapper->getSumOfPayedOutlays($this->teamMember);
+            $provided = $this->mapper->getSumOfProvidedButNotPayedOutlays($this->teamMember);
 
-        if ($budget > ($payed))
-        {
-            return true;
+            if ($budget > ($payed))
+            {
+                return true;
+            } else {
+                return false;
+
+            }
         } else {
             return false;
-
         }
     }
 
     public function hasCreditToProvide()
     {
-        $budget = $this->teamMember->getValue();
-        $payed = $this->mapper->getSumOfPayedOutlays($this->teamMember);
-        $provided = $this->mapper->getSumOfProvidedOutlays($this->teamMember);
+        if ($this->actionHasBeenApproved()) {
+            $budget = $this->teamMember->getValue();
+            $payed = $this->mapper->getSumOfPayedOutlays($this->teamMember);
+            $provided = $this->mapper->getSumOfProvidedOutlays($this->teamMember);
 
-        //die("$budget > $provided");
-        if ($budget > $provided)
-        {
-            return true;
+            //die("$budget > $provided");
+            if ($budget > $provided)
+            {
+                return true;
+            } else {
+                return false;
+
+            }
         } else {
             return false;
-
         }
     }
 
@@ -54,6 +62,23 @@ class C3op_Resources_TeamMemberHasCredit {
 
     }
 
+    private function actionHasBeenApproved()
+    {
+        $actionMapper = new C3op_Projects_ActionMapper;
+        $action = $actionMapper->findById($this->teamMember->getAction());
+
+        if (($action->getStatus() == C3op_Projects_ActionStatusConstants::STATUS_DONE)
+                || ($action->getStatus() == C3op_Projects_ActionStatusConstants::STATUS_DELIVERED)
+                || ($action->getStatus() == C3op_Projects_ActionStatusConstants::STATUS_COMPLETE)) {
+            return true;
+        } else {
+            return false;
+        }
+
+
+
+
+    }
+
 
 }
-
