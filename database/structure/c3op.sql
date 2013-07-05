2ba7e1c2fa808915c2bfc5650ed57ceb5920fd5c
Rodrigo Machado <rodrigo.aocubo@gmail.com>
2013-07-05 15:36:34 -0300

    updated database structure

--- database/structure/c3op.sql
+++ database/structure/c3op.sql
@@ -38,6 +38,46 @@ CREATE TABLE IF NOT EXISTS `auth_users` (
 INSERT INTO `auth_users` (`id`, `login`, `name`, `password`, `email`, `role`, `status`, `first_login`, `last_login`) VALUES
 (1, 'admin', '', '1cebede82348cd151992cc41693f795d', 'admin@localhost', 900, '', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
 
+--
+-- Estrutura da tabela `finances_outlays`
+--
+
+DROP TABLE IF EXISTS `finances_outlays`;
+CREATE TABLE IF NOT EXISTS `finances_outlays` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `project` int(10) unsigned DEFAULT NULL,
+  `action` int(10) unsigned DEFAULT NULL,
+  `responsible` int(10) unsigned DEFAULT NULL,
+  `predicted_value` decimal(10,2) DEFAULT NULL,
+  `predicted_date` date DEFAULT NULL,
+  `real_value` decimal(10,2) DEFAULT NULL,
+  `real_date` date DEFAULT NULL,
+  `recurrent` smallint(6) DEFAULT NULL,
+  `observation` varchar(255) CHARACTER SET latin1 COLLATE latin1_general_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `projeto` (`project`),
+  KEY `responsible` (`responsible`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `finances_receivables`
+--
+
+DROP TABLE IF EXISTS `finances_receivables`;
+CREATE TABLE IF NOT EXISTS `finances_receivables` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `project` int(10) unsigned DEFAULT NULL,
+  `title` varchar(200) CHARACTER SET latin1 COLLATE latin1_general_ci DEFAULT NULL,
+  `description` text,
+  `predicted_date` date DEFAULT NULL,
+  `real_date` date DEFAULT NULL,
+  `predicted_value` decimal(10,2) DEFAULT NULL,
+  `real_value` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
 -- --------------------------------------------------------
 
 --
@@ -53,9 +93,11 @@ CREATE TABLE IF NOT EXISTS `projects_actions` (
   `status` int(10) unsigned DEFAULT NULL,
   `description` text CHARACTER SET latin1 COLLATE latin1_general_ci,
   `subordinated_to` int(10) unsigned DEFAULT NULL,
-  `responsible` int(10) unsigned DEFAULT NULL,
+  `product` tinyint(1) unsigned DEFAULT NULL,
+  `supervisor` int(10) unsigned DEFAULT NULL,
   `milestone` tinyint(1) DEFAULT NULL,
   `requirement_for_receiving` int(10) unsigned DEFAULT NULL,
+  `budget_forecast` decimal(10,2) NOT NULL DEFAULT '0.00',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
@@ -69,6 +111,8 @@ DROP TABLE IF EXISTS `projects_actions_dates`;
 CREATE TABLE IF NOT EXISTS `projects_actions_dates` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `action` int(10) unsigned DEFAULT NULL,
+  `baseline_begin_date` date DEFAULT NULL,
+  `baseline_finish_date` date DEFAULT NULL,
   `predicted_begin_date` date DEFAULT NULL,
   `predicted_finish_date` date DEFAULT NULL,
   `real_begin_date` date DEFAULT NULL,
@@ -97,22 +141,44 @@ CREATE TABLE IF NOT EXISTS `projects_actions_events` (
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `projects_outlays`
+-- Estrutura da tabela `projects_contracts`
 --
 
-DROP TABLE IF EXISTS `projects_outlays`;
-CREATE TABLE IF NOT EXISTS `projects_outlays` (
+DROP TABLE IF EXISTS `projects_contracts`;
+CREATE TABLE IF NOT EXISTS `projects_contracts` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `project` int(10) unsigned DEFAULT NULL,
-  `action` int(10) unsigned DEFAULT NULL,
-  `human_resource` int(10) unsigned DEFAULT NULL,
-  `predicted_value` decimal(10,2) DEFAULT NULL,
+  `amendment` tinyint(1) DEFAULT NULL,
+  `begin_date` date NOT NULL,
+  `finish_date` date NOT NULL,
+  `signing_date` date DEFAULT NULL,
+  `value` decimal(10,2) DEFAULT NULL,
+  `contract_nature` tinyint(3) unsigned DEFAULT NULL,
+  `management_fee` decimal(10,2) DEFAULT NULL,
+  `object` text,
+  `summary` text,
+  `observation` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `projects_deliveries`
+--
+
+DROP TABLE IF EXISTS `projects_deliveries`;
+CREATE TABLE IF NOT EXISTS `projects_deliveries` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `project` int(10) unsigned NOT NULL,
+  `receivable` int(10) unsigned NOT NULL,
+  `baseline_date` date DEFAULT NULL,
   `predicted_date` date DEFAULT NULL,
-  `recurrent` smallint(6) DEFAULT NULL,
-  `observation` varchar(255) CHARACTER SET latin1 COLLATE latin1_general_ci DEFAULT NULL,
+  `real_date` date DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `projeto` (`project`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
+  KEY `receivable` (`receivable`),
+  KEY `project` (`project`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -140,42 +206,7 @@ CREATE TABLE IF NOT EXISTS `projects_projects` (
   `summary` text,
   `observation` text,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `projects_receivables`
---
-
-DROP TABLE IF EXISTS `projects_receivables`;
-CREATE TABLE IF NOT EXISTS `projects_receivables` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `project` int(10) unsigned DEFAULT NULL,
-  `title` varchar(200) CHARACTER SET latin1 COLLATE latin1_general_ci DEFAULT NULL,
-  `predicted_date` date DEFAULT NULL,
-  `real_date` date DEFAULT NULL,
-  `predicted_value` decimal(10,2) DEFAULT NULL,
-  `real_value` decimal(10,2) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `projects_team_members`
---
-
-DROP TABLE IF EXISTS `projects_team_members`;
-CREATE TABLE IF NOT EXISTS `projects_team_members` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `action` int(10) unsigned NOT NULL,
-  `description` varchar(120) CHARACTER SET latin1 COLLATE latin1_general_ci DEFAULT NULL,
-  `contact` int(10) unsigned NOT NULL,
-  `value` decimal(10,2) unsigned NOT NULL,
-  `status` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -189,7 +220,7 @@ CREATE TABLE IF NOT EXISTS `register_contacts` (
   `name` varchar(150) DEFAULT NULL,
   `type` smallint(5) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -205,7 +236,7 @@ CREATE TABLE IF NOT EXISTS `register_contacts_emails` (
   `label` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `contact` (`contact`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -221,7 +252,7 @@ CREATE TABLE IF NOT EXISTS `register_contacts_messengers` (
   `service` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `contact` (`contact`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -238,7 +269,7 @@ CREATE TABLE IF NOT EXISTS `register_contacts_phone_numbers` (
   `label` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `contact` (`contact`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -266,7 +297,7 @@ CREATE TABLE IF NOT EXISTS `register_institutions` (
   `type` smallint(5) unsigned DEFAULT NULL,
   `relationship_type` smallint(5) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -283,7 +314,7 @@ CREATE TABLE IF NOT EXISTS `register_linkages` (
   `position` varchar(120) CHARACTER SET latin1 COLLATE latin1_general_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `contato` (`contact`,`institution`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -299,7 +330,7 @@ CREATE TABLE IF NOT EXISTS `register_linkages_emails` (
   `label` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `linkage` (`linkage`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -316,4 +347,51 @@ CREATE TABLE IF NOT EXISTS `register_linkages_phone_numbers` (
   `label` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `linkage` (`linkage`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `resources_material_supplies`
+--
+
+DROP TABLE IF EXISTS `resources_material_supplies`;
+CREATE TABLE IF NOT EXISTS `resources_material_supplies` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `project` int(10) unsigned NOT NULL,
+  `action` int(10) unsigned NOT NULL,
+  `description` varchar(120) CHARACTER SET latin1 COLLATE latin1_general_ci DEFAULT NULL,
+  `institution` int(10) unsigned DEFAULT NULL,
+  `linkage` int(10) unsigned NOT NULL,
+  `quantity` int(10) unsigned NOT NULL,
+  `unit` varchar(120) DEFAULT NULL,
+  `unit_value` decimal(10,2) NOT NULL,
+  `total_value` decimal(10,2) unsigned NOT NULL,
+  `status` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `action` (`action`),
+  KEY `status` (`status`),
+  KEY `institution` (`institution`),
+  KEY `project` (`project`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `resources_responsibles`
+--
+
+DROP TABLE IF EXISTS `resources_responsibles`;
+CREATE TABLE IF NOT EXISTS `resources_responsibles` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `project` int(10) unsigned NOT NULL,
+  `action` int(10) unsigned NOT NULL,
+  `type` int(10) unsigned NOT NULL,
+  `institution` int(10) unsigned NOT NULL,
+  `contact` int(10) unsigned NOT NULL,
+  `predicted_value` decimal(10,2) unsigned DEFAULT NULL,
+  `contracted_value` decimal(10,2) unsigned DEFAULT NULL,
+  `status` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `project` (`project`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
